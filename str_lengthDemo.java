a3f41ee53b8dabc77f0b2516a18d7bffcc1ac2d9
Preeti Sharma <preeti19sharma94@gmail.com>
2017-02-02 10:10:02 +0530

    Add files via upload

---
+++ str_lengthDemo.java
@@ -0,0 +1,10 @@
+class str_lengthDemo
+{
+	 public static void main(String[] args) {
+	 	char str[] = {'P','R','O','G','R','A','M'};
+	 	String S  = new String(str);
+	 	System.out.println("The string S is " +S);
+	 	System.out.println("The length of string is" +S.length());
+		
+	}
+}
