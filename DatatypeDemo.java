d08b19702b2c90751b6db5883d5781c69c0f38b2
Preeti Sharma <preeti19sharma94@gmail.com>
2017-02-02 07:37:29 +0530

    Add files via upload

---
+++ DatatypeDemo.java
@@ -0,0 +1,21 @@
+class DatatypeDemo 
+{
+	public static void main(String agrs[])
+	{
+	byte a = 10;
+	short b = 10 * 23;
+	int c = 10000 * 128;
+	long d = 10000* 1000* 128;
+	double e = 99.9998;
+	char f = 'a';
+	boolean g = true;
+	boolean h = false;
+	System.out.println("The value of a =" +a);
+	System.out.println("The value of b =" + b);
+	System.out.println("The value of c =" +c);
+	System.out.println("The value of d =" + d);
+	System.out.println("The value of e =" +e);
+	System.out.println("The value of f =" + f);
+	
+      }
+}
